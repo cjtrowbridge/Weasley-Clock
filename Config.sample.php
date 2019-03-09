a37a04cb7e487c010478d1cabc0b2cbaa1df1926
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-03-09 13:06:42 -0800

    Update Config.sample.php

--- Config.sample.php
+++ Config.sample.php
@@ -1,3 +1,17 @@
 <?php
 
-$Key="";
+$People = array(
+  'Person One' => array(
+    'Photo' => 'https://graph.facebook.com/[FACEBOOK ID]/picture?width=9999&height=9999',
+    'Key'   => ''
+  ),
+  'Person Two' => array(
+    'Photo' => 'https://graph.facebook.com/[FACEBOOK ID]/picture?width=9999&height=9999',
+    'Key'   => ''
+  )
+);
+
+$Places = array(
+  'Home',
+  'Away'
+);
